def625480aec3aaa6618261b8698f25a020a9723
Inyermansp <inyersp@gmail.com>
2022-11-26 15:19:32 -0500

    Ejercicio 3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Model
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese un numero");
+            int resultado = int.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado es: ");
+            Console.WriteLine( resultado - (resultado * 15 / 100));
+           
+
+
+
+
+
+        }
+
+
+    }
+}
